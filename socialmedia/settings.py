186efbf6d09ca34729dda1f48092097d49c304c8
CHRISTOPHER ANCHETA <christopherancheta@CHRISTOPHERs-Mac-mini.local>
2025-05-14 18:10:59 +1200

    Fixed Logout

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -52,8 +52,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # Temporarily disable CSRF for development
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
