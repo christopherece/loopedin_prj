d7cd6b46b38dfe4732b72253f2a347f5279a76f0
Christopher Ancheta <christopherancheta@Christophers-MacBook-Pro.local>
2025-06-22 23:16:28 +1200

    Modifed Link

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -166,9 +166,9 @@ USE_TZ = True
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'topitsolutionsnz@gmail.com'
 EMAIL_HOST_PASSWORD = 'tlyp wlck iipl lrll'
 
